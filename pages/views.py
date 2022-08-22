7ee7a8ea855983d351e349ecdfc6d0dfb1a2cf60
temesgen-zenebe <temf2006@gmail.com>
2022-08-21 18:55:31 -0700

    editing home page and get the number of vistiros  and render to the home page

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from atexit import register
 import email
+import numbers
 from typing import ValuesView
 from django.views.generic import TemplateView,CreateView,ListView
 from django.views import View
@@ -10,7 +11,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse,HttpResponseRedirect
 from anagram_game.models import Anagram_score,Comment_Anagram
 from math_game.models import Addition_score,Division_score,Multiplication_score,Subtraction_score,Comment_math
-
+from django.contrib.auth.models import User
 from anagram_game.forms import CommentAnagrameForm
 from math_game.forms import CommentForm 
 from users.models import LoggedUser 
@@ -20,7 +21,12 @@ from .forms import GameCommentForm
 from django.core.exceptions import ValidationError
 from django.db.models import Q
 from django.template import loader
+from datetime import date, datetime,timedelta
+from django.utils import timezone
 
+today = timezone.now()
+startDate = today - timedelta(days=7)
+endDate = today 
 
 
 class HomePageView(TemplateView):
@@ -48,7 +54,7 @@ class MyCommentsListView(View):
      
      def post(self, request):
         game = request.POST.get('game_type')
-        print(game)
+        #print(game)
         if game == 'anagrame':# anagrame game
             data = {  
                 'game_type' : request.POST.get('game_type'),
@@ -93,16 +99,27 @@ class GameCommentListView(ListView):
        return comment
     
 class PrintGameScore(View):
+   
     def get(self, request):
-        #operate10 = Addition_score.objects.filter(Q(user = request.user) & Q(max_range = 10)).order_by('-point')
+        
+        #about math game
         operate1 = Addition_score.objects.all().order_by('-point').distinct('point')
         operate2 = Division_score.objects.all().order_by('-point').distinct('point')
         operate3=  Multiplication_score.objects.all().order_by('-point').distinct('point')
         operate4 = Subtraction_score.objects.all().order_by('-point').distinct('point')
+        # about 
         anagramScore = Anagram_score.objects.all().order_by('-point').distinct('point')
+        
+        #comment
         comment = Games_comment.objects.filter(Q(active = True)).order_by('-created').distinct('created')
+        
+        #user information 
         logged_users=LoggedUser.objects.all()
-       
+        numbers_users = get_user_model().objects.all().count()
+        weekly_signup = get_user_model().objects.filter(date_joined__range=[startDate,endDate]).count()
+        #print(weekly_signup)
+        num_visits = request.session.get('num_visits', 0)
+        request.session['num_visits'] = num_visits + 1
         context = {
             'Addition':operate1,
             'Division':operate2,
@@ -111,8 +128,10 @@ class PrintGameScore(View):
             'anagramScore':anagramScore,
             'gameComment':comment,
             'logged_users': logged_users,
-            
-            
+            'numbers_users': numbers_users,
+            'weekly_signup':weekly_signup,
+            'num_visits': num_visits
+        
         }
         return render(request, 'pages/home.html', context)
     
@@ -131,8 +150,8 @@ class PrintGameScore(View):
             return redirect('pages:math-score-list')
         else:   
             return render(request,"pages/home.html",{})
-
         
+       
 def deleteCommentMath(request, id):
   member = Comment_math.objects.get(id=id)
   member.delete()
