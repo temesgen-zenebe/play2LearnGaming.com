92c4ecc91dd79fefedef30752fd4ed3485826abf
temesgen-zenebe <temf2006@gmail.com>
2022-11-05 18:12:53 -0700

    change in setting

--- play2learnGames/wsgi.py
+++ play2learnGames/wsgi.py
@@ -7,15 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-import os,sys
+import os
 
 from django.core.wsgi import get_wsgi_application
-
-path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-if path not in sys.path:
-    sys.path.append(path)
-    
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'play2learnGames.settings')
-
 application = get_wsgi_application()
