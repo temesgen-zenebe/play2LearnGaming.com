dab713b9cfeb631efa8c057dd46e19d7a7439986
temesgen-zenebe <temf2006@gmail.com>
2022-11-06 13:19:43 -0800

    static file

--- play2learnGames/settings.py
+++ play2learnGames/settings.py
@@ -192,7 +192,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
