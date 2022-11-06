cc5d1a41c8bbb0fdc4975885a57d30897c59bb7e
temesgen-zenebe <temf2006@gmail.com>
2022-11-05 22:08:41 -0700

    setting.py setteing

--- play2learnGames/settings.py
+++ play2learnGames/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    
     # Third-party
     'crispy_forms',
     'allauth',
@@ -180,6 +181,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
